0c8e0d8b825096b0762f64ffe95b7868bf811df6
zhouleigang <472893Gang>
2020-06-12 13:03:34 +0800

    config task.json launch.json for vscode env

--- compile-windows.bat
+++ compile-windows.bat
@@ -6,11 +6,9 @@ SETLOCAL
 IF "%1"=="32" (set Machine=x86) else (set Machine=x64)
 set ProjName=%2
 set Config=%3
-set platform=%4
 echo Machine: %Machine%
 echo ProjName: %ProjName%
 echo Config: %Config%
-echo Platform:%platform%
 
 set Local_Path=%~dp0%
 echo LocalPath: %Local_Path%
@@ -18,5 +16,5 @@ cd %Local_Path%
 set VCINSTALLDIR=C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build
 set PATH=%VCINSTALLDIR%\bin;%PATH%
 call "%VCINSTALLDIR%\vcvarsall.bat" %Machine%
-msbuild %ProjName% /t:Rebuild /p:Configuration=%Config% /p:Platform=%platform% /p:SubsystemVersion=5.1
+msbuild %ProjName% /t:Rebuild /p:Configuration=%Config% /p:Platform=%Machine% /p:SubsystemVersion=5.1
 
